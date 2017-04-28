8e540e5ab1a424fbc22bea737c373b845680b890
TZK- <thib-17@orange.fr>
2017-04-28 13:33:01 +0200

    Updating to get it work with TaigaPHP v1.1.0

--- config/taiga.php
+++ config/taiga.php
@@ -1,7 +1,9 @@
 <?php
 
 return [
-    'api'      => env('TAIGA_API', 'https://api.taiga.io/api/v1/'),
-    'token'    => env('TAIGA_TOKEN', null),
-    'language' => env('TAIGA_LANGUAGE', 'en'),
+    'api' => env('TAIGA_API', 'https://api.taiga.io/api/v1/'),
+    'token' => env('TAIGA_TOKEN'),
+    'headers' => [
+    	'language' => env('TAIGA_LANGUAGE', 'en')
+    ]
 ];
--- src/Facades/Taiga.php
+++ src/Facades/Taiga.php
@@ -2,12 +2,18 @@
 
 namespace TZK\TaigaLaravel\Facades;
 
+use TZK\Taiga\Taiga as TaigaPHP;
+
 class Taiga extends \Illuminate\Support\Facades\Facade
 {
 
     protected static function getFacadeAccessor()
     {
-        return \TZK\Taiga\Taiga::class;
+        return TaigaPHP::class;
+    }
+
+    public static function newInstance() {
+    	return app(TaigaPHP::class);
     }
 
 }
